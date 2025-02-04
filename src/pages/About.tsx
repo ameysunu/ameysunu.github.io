920797f1ed78216e60576cf68f6eff49e532e737
ameysunu <ameysunu00719@gmail.com>
2025-02-04 21:57:52 +0000

    about added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,10 +1,31 @@
+import { motion } from "motion/react";
 import Header from "./Header";
+import { useEffect, useState } from "react";
 
 export default function About() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    useEffect(() => {
+        setIsVisible(true);
+    }, []);
+
     return (
         <div>
             <Header />
-            <p>Hello</p>
+            <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: isVisible ? 1 : 0, y: isVisible ? 0 : -20 }}
+                transition={{ duration: 0.5 }}
+            >
+                <div className="home">
+                    <h1 className="big-heading">
+                        I'm Amey.
+                    </h1>
+                </div>
+
+            </motion.div>
+
         </div>
     );
 }
