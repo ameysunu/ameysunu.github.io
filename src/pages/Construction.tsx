2152b79d3bc031726bf1d759c8745b9eb63c063f
ameysunu <ameysunu00719@gmail.com>
2025-08-08 15:44:18 +0100

    image width decreased

--- src/pages/Construction.tsx
+++ src/pages/Construction.tsx
@@ -3,7 +3,7 @@ import underconstruction from '../assets/underconstruction.png';
 export default function Construction() {
     return (
       <div className="not-found">
-        <img src={underconstruction} style={{ width: '100%', height: 'auto', maxWidth: '600px', marginBottom: '20px' }}></img>
+        <img src={underconstruction} style={{ width: '100%', height: 'auto', maxWidth: '400px', marginBottom: '5px' }}></img>
         <h1 style={{ fontSize: '3rem', color: 'yellow' }}>
           This page is under construction.
         </h1>
