277aaad34b653601c52990b049c60f7d4cb9f062
ameysunu <ameysunu00719@gmail.com>
2025-09-26 20:58:24 +0100

    blogs url added

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -12,7 +12,7 @@ export default function Header({ onContactClick }: HeaderProps) {
                 <NavLink to ="/" className="header-box ">Me</NavLink>
                 <NavLink to ="/about" className="header-box ">About</NavLink>
                 <NavLink to ="/stuff" className="header-box ">Stuff</NavLink>
-                <NavLink to ="/under-construction" className="header-box ">Blog</NavLink>
+                <NavLink to ="/blogs" className="header-box ">Blogs</NavLink>
                 <a onClick={onContactClick} className="header-box" style={{ cursor: "pointer"}}> Contact </a>
             </div>
         </div>
